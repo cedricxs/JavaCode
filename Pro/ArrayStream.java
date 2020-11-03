ebb25df0ca4f1ee8231f9a938ff500d169e2a10b
Cedricxs <979581491@qq.com>
2020-11-04 05:06:33 +0800

    Update ArrayStream.java

--- Pro/ArrayStream.java
+++ Pro/ArrayStream.java
@@ -57,10 +57,8 @@ public class ArrayStream {
 		Optional<String> max = stream.max(new Comparator<String>() {
 			@Override
 			public int compare(String o1, String o2) {
-				if(o1.length()>o2.length())
-					return 1;
-				if(o1.length()<o2.length())
-					return -1;
+				if(o1.length()!=o2.length())
+					return o1.length()-o2.length();
 				if(o1.length() == o2.length()) {
 					for(int i=0;i<o1.length();i++)
 						if(o1.charAt(i) != o2.charAt(i))
